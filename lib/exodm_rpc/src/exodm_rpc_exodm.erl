02652308405a037563d88c9c80180bf25bb229c5
Magnus Feuer <magnus@feuerlabs.com>
2012-08-15 13:59:58 -0700

    Normalized admin json-rpc elements. Added add-config-data-members.

--- lib/exodm_rpc/src/exodm_rpc_exodm.erl
+++ lib/exodm_rpc/src/exodm_rpc_exodm.erl
@@ -19,16 +19,20 @@ result_code('object-exists') ->
     [{result, <<"object-exists">>}];
 
 result_code('object-not-found') ->
-    [{result, <<"object-not-found">>}].
+    [{result, <<"object-not-found">>}];
+
+result_code('device-not-found') ->
+    [{result, <<"device-not-found">>}].
 
 
 json_rpc(RPC, Env) ->
     ?debug("~p:json_rpc(~p, ~p)~n", [?MODULE, RPC, Env]),
     json_rpc_(RPC, Env).
 
+
 json_rpc_({request, _ReqEnv,
 	   {call, exodm, 'create-config-data',
-	    [{name, N},
+	    [{'config-data', N},
 	     {yang, Y},
 	     {protocol, P},
 	     {values, JSON}]}} = _RPC, _Env) ->
@@ -44,11 +48,12 @@ json_rpc_({request, _ReqEnv,
 	    {error, E}
     end;
 
+
 json_rpc_({request, _ReqEnv,
-	   {call, exodm, 'set-config-value',
-	    [{'cfg-data', C},
+	   {call, exodm, 'update-config-data',
+	    [{'config-data', C},
 	     {values, JSON}]}} = RPC, _Env) ->
-    ?debug("set-config-data: cfg-data: ~p values:~p~n", [C, JSON]),
+    ?debug("update-config-data: config-data: ~p values:~p~n", [C, JSON]),
     AID = exodm_db_session:get_aid(),
     case exodm_db_config:update_config_data(AID, C, JSON) of
 	{ok, _} ->
@@ -75,23 +80,35 @@ json_rpc_({request, _ReqEnv,
 
 json_rpc_({request, _ReqEnv,
 	   {call, exodm, 'provision-device',
-	    [{'new-device-id', I}]}} = _RPC, _Env) ->
+	    [{'dev-id', I}]}} = _RPC, _Env) ->
     ?debug("provision-device() device-id():~p ~n", [ I ]),
     exodm_db_device:new(exodm_db_session:get_aid(), I, []),
     {ok, result_code(ok)};
 
+
 json_rpc_({request, _ReqEnv,
 	   {call, exodm, 'add-config-data-members',
-	    [{repository, R},
-	     {name, N},
-	     {'yang-module', Y}]}} = _RPC, _Env) ->
-    ?debug("create-yang-module~n", []),
-    io:format("YANG WRONG REPO: Name:~p Repo:~p Yang:~p\n", [N,R,Y]),
-    {ok, result_code('permission-denied') };
+	    [{'config-data', {array, CfgList}},
+	     {'dev-id', {array, DevList}}]}} = _RPC, _Env) ->
+    ?debug("add-config-data-members~n", []),
+    AID = exodm_db_session:get_aid(),
+    F = fun(CfgData) ->
+                exodm_db_config:add_config_data_members(AID, CfgData, DevList)
+        end,
+    try [ F(CfgData) || CfgData <- CfgList ] of
+        _ ->  {ok, result_code(ok) }
+    catch error: { unknown_config_data, CfgData} ->
+            ?info("add-config-data-members(): Unknown config data: ~p\n",
+                  [CfgData]),
+	    {ok, result_code('object-not-found') };
+
+          error: { unknown_device, Dev} ->
+            ?info("add-config-data-members(): Unknown device-id ~p cannot be added to config data\n",
+                  [Dev]),
+	    {ok, result_code('device-not-found') }
+    end;
 
 json_rpc_(RPC, _ENV) ->
-
-
     ?error("~p:json_rpc_() Unknown RPC: ~p ~n", [ ?MODULE, RPC ]),
     {ok, result_code('validation-failed')}.
 
