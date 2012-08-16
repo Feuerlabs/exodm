bceef33f26990f95d899732c903911bb95a3f62e
Magnus Feuer <magnus@feuerlabs.com>
2012-08-15 19:06:28 -0700

    provision-device now takes server and client keys.

--- lib/exodm_rpc/src/exodm_rpc_exodm.erl
+++ lib/exodm_rpc/src/exodm_rpc_exodm.erl
@@ -36,7 +36,8 @@ json_rpc_({request, _ReqEnv,
 	     {yang, Y},
 	     {protocol, P},
 	     {values, JSON}]}} = _RPC, _Env) ->
-    ?debug("'create-config-data'~n", []),
+    ?debug("~p:json_rpc(create-config-data) config-data:~p yang:~p protocol:~p values:~p~n",
+           [ ?MODULE, N, Y, P, JSON ]),
     AID = exodm_db_session:get_aid(),
     try exodm_db_config:new_config_data(AID, N, Y, P, JSON) of
 	{ok, _} ->
@@ -53,7 +54,9 @@ json_rpc_({request, _ReqEnv,
 	   {call, exodm, 'update-config-data',
 	    [{'config-data', C},
 	     {values, JSON}]}} = RPC, _Env) ->
-    ?debug("update-config-data: config-data: ~p values:~p~n", [C, JSON]),
+    ?debug("~p:json_rpc(update-config-data) config-data:~p values:~p~n",
+           [ ?MODULE, C, JSON ]),
+
     AID = exodm_db_session:get_aid(),
     case exodm_db_config:update_config_data(AID, C, JSON) of
 	{ok, _} ->
@@ -71,44 +74,32 @@ json_rpc_({request, _ReqEnv,
 	    [{repository, R},
 	     {name, N},
 	     {'yang-module', Y}]}} = _RPC, _Env) when R =:= ?USER_REPOSITORY->
-    ?debug("'create-yang-module'~n", []),
-
-    Res = exodm_db_yang:write(N, Y),
-    io:format("YANG WRITE: ~p\n", [Res]),
+    ?debug("~p:json_rpc(create-yang-module) repository:~p name:~p~n", [ ?MODULE, R, N ]),
+    _Res = exodm_db_yang:write(N, Y),
     {ok, result_code(ok)};
 
 
 json_rpc_({request, _ReqEnv,
 	   {call, exodm, 'provision-device',
-	    [{'dev-id', I}]}} = _RPC, _Env) ->
-    ?debug("provision-device() device-id():~p ~n", [ I ]),
-    exodm_db_device:new(exodm_db_session:get_aid(), I, []),
+	    [{'dev-id', I},
+             {'server-key', SK},
+             {'device-key', DK}]}} = _RPC, _Env) ->
+    ?debug("~p:json_rpc(provision-device) device-id:~p server-key:~p device-key:~p~n",
+           [ ?MODULE, I, SK, DK ]),
+
+    exodm_db_device:new(exodm_db_session:get_aid(), I, [{'__ck', <<DK:64/little>>},
+                                                        {'__sk', <<SK:64/little>>}]),
     {ok, result_code(ok)};
 
 
 json_rpc_({request, _ReqEnv,
-	   {call, exodm, 'add-config-data-members',
-	    [{'config-data', {array, CfgList}},
-	     {'dev-id', {array, DevList}}]}} = _RPC, _Env) ->
-    ?debug("add-config-data-members~n", []),
-    AID = exodm_db_session:get_aid(),
-    F = fun(CfgData) ->
-                exodm_db_config:add_config_data_members(AID, CfgData, DevList)
-        end,
-    try [ F(CfgData) || CfgData <- CfgList ] of
-        _ ->  {ok, result_code(ok) }
-    catch error: { unknown_config_data, CfgData} ->
-            ?info("add-config-data-members(): Unknown config data: ~p\n",
-                  [CfgData]),
-	    {ok, result_code('object-not-found') };
-
-          error: { unknown_device, Dev} ->
-            ?info("add-config-data-members(): Unknown device-id ~p cannot be added to config data\n",
-                  [Dev]),
-	    {ok, result_code('device-not-found') }
-    end;
+	   {call, exodm, 'push-config-data',
+	    [{'config-data', Cfg}]}} = _RPC, _Env) ->
+    ?debug("~p:json_rpc(push-config-data) config-data:~p ~n", [ ?MODULE, Cfg ]),
+    _AID = exodm_db_session:get_aid(),
+    {ok, result_code(ok)};
 
 json_rpc_(RPC, _ENV) ->
-    ?error("~p:json_rpc_() Unknown RPC: ~p ~n", [ ?MODULE, RPC ]),
+    ?info("~p:json_rpc_() Unknown RPC: ~p ~n", [ ?MODULE, RPC ]),
     {ok, result_code('validation-failed')}.
 
