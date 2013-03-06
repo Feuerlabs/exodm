fb7979a475ec858ebc6dce8779c5bcc4934e42d1
Ulf Wiger <ulf@feuerlabs.com>
2013-03-06 17:56:12 +0100

    changes to plugin API

--- src/exodm_plugin.erl
+++ src/exodm_plugin.erl
@@ -1,8 +1,9 @@
 -module(exodm_plugin).
 
 -export([add_http_session/0,
-	 login/1,
-	 logout/0, logout/1]).
+	 login/2,
+	 logout/0, logout/1,
+	 get_account/0]).
 
 -export([get_cached_config/3]).
 
@@ -16,8 +17,8 @@ add_http_session() ->
 %% `{exodm_db_user, delete, AnyUser}' (note: not just for the current user).
 %% This allows the process to stop performing operations that rely on the
 %% presence of `User'.
-login(User) ->
-    login_(User, 3).
+login(Account, User) ->
+    login_(Account, User, 3).
 
 %% By default, this call will activate a subscription on user add events.
 %% I.e. if a user is added/deleted, the current process will receive messages
@@ -37,6 +38,9 @@ logout(Resubscribe) when is_boolean(Resubscribe) ->
     end,
     ok.
 
+get_account() ->
+    exodm_db_session:get_aid().
+
 get_cached_config(ConfigSet, Ref, DeviceID) ->
     AID = exodm_db_session:get_aid(),
     exodm_db_config:get_cached(AID, ConfigSet, Ref, DeviceID).
@@ -45,16 +49,35 @@ device_exists(DID) ->
     exodm_db_device:exist(exodm_db_session:get_aid(), DID).
 
 
-login_(User, Retries) when is_integer(Retries) ->
-    case exodm_db_session:set_auth_as_user(User, kvdb_conf, _Sticky=true) of
-	false when Retries > 0 ->
-	    timer:sleep(500),
-	    login_(User, Retries-1);
+login_(Account, User, Retries) when is_integer(Retries) ->
+    case get_account_id(Account) of
+	AID when is_binary(AID) ->
+	    case exodm_db_session:set_auth_as_user(
+		   AID, User, kvdb_conf, _Sticky=true) of
+		false when Retries > 0 ->
+		    timer:sleep(500),
+		    login_(Account, User, Retries-1);
+		false ->
+		    false;
+		{true,_,_} ->
+		    exodm_db_user:unsubscribe(add),
+		    catch exodm_db_user:subscribe(delete),
+		    exodm_db_session:set_trusted_proc(),
+		    true
+	    end;
 	false ->
-	    false;
-	{true,_,_} ->
-	    exodm_db_user:unsubscribe(add),
-	    catch exodm_db_user:subscribe(delete),
-	    exodm_db_session:set_trusted_proc(),
-	    true
+	    false
+    end.
+
+get_account_id(Acct) ->
+    case exodm_db_account:exist(Acct) of
+	false ->
+	    case exodm_db_account:lookup_by_name(Acct) of
+		AID when is_binary(AID) ->
+		    AID;
+		false ->
+		    false
+	    end;
+	true ->
+	    Acct
     end.
